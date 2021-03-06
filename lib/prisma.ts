e293f2d5b653eec2fd9c1e16fadfdefc5af3d320
Sanmati Kumar Jain <i.sanmati.jain@redcarpetup.com>
2021-03-07 04:48:22 +0530

    add prisma client, schema

---
+++ lib/prisma.ts
@@ -0,0 +1,16 @@
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
+
+const globalAny:any = global;
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!globalAny.prisma) {
+    globalAny.prisma = new PrismaClient()
+  }
+  prisma = globalAny.prisma
+}
+
+export default prisma
